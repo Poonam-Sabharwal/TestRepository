4e61b769bd60e23fd969f600a210f0be37696664
saikiranreddy098 <saikiranreddy@aarkglobalinc.com>
2023-08-11 15:46:06 +0530

    Changed few files and added one more module named dataobjects

--- apps/common/utils.py
+++ apps/common/utils.py
@@ -1,8 +1,11 @@
 import logging
+from datetime import datetime
 import os
-from apps.common import config_reader
-from apps.common.custom_exceptions import MissingDocumentTypeException
-
+from apps.common import config_reader,constants
+from apps.common.custom_exceptions import MissingDocumentTypeException,MissingConfigException
+import base64
+from common.data_objects import Metadata
+from azure.storage.blob import BlobServiceClient
 countries_dict = {
     "-1": "Select a Country",
     "US": "United States",
@@ -87,6 +90,18 @@ ca_states_dict = {
     "YT": "Yukon",
 }
 
+def connect_str():
+    if not config_reader.config_data.has_option("Main", "azure-storage-account-connection-str"):
+        raise MissingConfigException("Main.azure-storage-account-connection-str is missing in config.")
+    azure_storage_account_connection_str = config_reader.config_data.get("Main", "azure-storage-account-connection-str")
+    if not string_is_not_empty(azure_storage_account_connection_str):
+        raise MissingConfigException("Main.azure-storage-account-connection-str is present but has empty value.")
+    if azure_storage_account_connection_str.startswith(("'", '"')) and azure_storage_account_connection_str.endswith(
+        ("'", '"')
+    ):
+        azure_storage_account_connection_str = azure_storage_account_connection_str.strip("'\"")
+    return azure_storage_account_connection_str
+
 
 def string_is_not_empty(input_str):
     return (input_str is not None) and len(input_str) > 0
@@ -97,7 +112,7 @@ def is_env_local():
         env = config_reader.config_data.get("Main", "env")
     else:
         env = "local"
-
+        
     return env.lower() == "local".lower()
 
 
@@ -157,3 +172,58 @@ def get_document_type_from_file_name(file_path):
         msg = f"Could not find form recognizer model for document type {document_type} inferred form file name path {file_path}."
         logging.error(msg)
         raise MissingDocumentTypeException(msg)
+    
+def blob_service_client():
+    """
+    blob_service_client calls BobServiceClient
+
+    Returns:
+        BobServiceClient
+    """
+    return BlobServiceClient.from_connection_string(connect_str())
+
+def container_client():
+    """
+    container_client calls ContainerClient
+
+    Returns:
+        ContainerClient
+    """
+    return blob_service_client().get_container_client(constants.DEFAULT_BLOB_CONTAINER)
+
+def get_metadata(status: str, path: str):
+    """
+    get_meta takes file status and its path and collects metadata properties of that blob.
+
+    Args:
+        status (str): Blob status
+        path (str): Path of blob
+    Returns:
+      str:  returns object of class Metadata
+    """
+    blob_client = container_client().get_blob_client(path)
+    properties = blob_client.get_blob_properties()
+
+    md = Metadata()
+    
+    md.status = f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}-{status}]"
+    md.name = properties.name
+    md.content_md5 = base64.b64encode(properties.content_settings.content_md5).decode("utf-8")
+    md.url = blob_client.url
+    md.blob_type = properties.blob_type
+    md.container = container_client().container_name
+    md.content_length = properties.size
+    md.created = properties.creation_time.strftime("%Y-%m-%d %H:%M:%S")
+    md.last_modified = properties.last_modified.strftime("%Y-%m-%d %H:%M:%S")
+    md.content_type = properties.content_settings.content_type
+    
+    return md
+
+############################################
+
+
+
+
+
+
+
